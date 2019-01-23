8a7d4d6c3f6611386ab1dda880f82963b87b3205
Marcin Webmaster <buczynski.programuje@gmail.com>
2019-01-23 16:51:47 +0100

    Odcinek 6

--- hello/Program.cs
+++ hello/Program.cs
@@ -6,14 +6,58 @@ namespace hello
     {
         static void Main(string[] args)
         {
-            //string name = args[0];
-            //Console.WriteLine("Hello! " + name);
-
-            foreach (var item in args)
+            for (; ; )
             {
-                Console.WriteLine("Witaj, " + item );
+                Greeting();
+                Age();
+                Settings();
+
             }
-        
+
+        }
+        /// <summary>
+        /// Resetujemy ustawienia konsoli
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+        /// <summary>
+        /// Pytamy użytkownika o jego wiek
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Podaj ile masz lat: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Jesteś pełnoletni, możesz wypić browara :)");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Wprowadziłeś niepoprawnie wiek !");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Napij się mleka dzieciaku! xD");
+
+            }
+        }
+        /// <summary>
+        /// Wypisujemy powitanie użytkownika
+        /// </summary>
+        private static void Greeting()
+        {
+            Console.Write("Wprowadź swoje imię: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Cześć! " + name);
         }
     }
 }
+//33:00/42:37 Odcinek 6)

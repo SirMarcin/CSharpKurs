9c79d0b74f4cef898aced79a04c7e1caef875984
Marcin Webmaster <buczynski.programuje@gmail.com>
2019-01-29 16:31:13 +0100

    responsibility

---
+++ Odcinek10/Diary.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Diary
+    {
+
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+
+        //Stan
+        List<float> ratings;
+
+        internal DiaryStatistics ComputStatistics()
+        {
+
+            DiaryStatistics stats = new DiaryStatistics();
+
+            float sum = 0f;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            stats.AverageGrade = sum / ratings.Count();
+            stats.MaxGrade = ratings.Max();
+            stats.MinGrade = ratings.Min();
+            
+            return stats;
+        }
+
+
+        /// <summary>
+        /// Dodawanie nowej oceny
+        /// </summary>
+        /// <param name="rating"></param>
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ Odcinek10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary;
+            diary = new Diary();
+            diary.AddRating(3);
+            diary.AddRating(5);
+            diary.AddRating(2);
+            diary.AddRating(2);
+
+            DiaryStatistics stats = diary.ComputStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.MinGrade);
+            Console.WriteLine(stats.MaxGrade);
+
+            Diary diary2;
+            diary2 = new Diary();
+            diary2.AddRating(3);
+            diary2.AddRating(4);
+            diary2.AddRating(1);
+            diary2.AddRating(1);
+
+            stats = diary2.ComputStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.MinGrade);
+            Console.WriteLine(stats.MaxGrade);
+
+        }
+    }
+}
